00c48f6716ae92e52221da82e1ababa0460acf62
Osvaldo Murillo <osvaldo.murillo29@gmail.com>
2020-02-26 16:41:28 -0500

    Initial Commit

---
+++ ImageCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageCoreData
+//
+//  Created by Osvaldo Murillo on 2/26/20.
+//  Copyright © 2020 Osvaldo Murillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
